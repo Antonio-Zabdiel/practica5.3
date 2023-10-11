46277e5e9ed45f6ffaa055126f550b3a96664a16
Antonio-Zabdiel <112569142+Antonio-Zabdiel@users.noreply.github.com>
2023-10-11 16:54:14 -0600

    programa corregido

--- main.c
+++ main.c
@@ -25,9 +25,24 @@ int main(){
         }
     }
     
-    for(l = 0 ; l <=50 ; l++){
-        printf("%d\t%d\n",numeros_par[52],numeros_inp[l]);
+    printf("numeros pares            numeros inpares");
+
+    while (numeros_inp[l] != '\0')
+    {
+        //printf("%d\t",numeros_par[l]);
+
+        if (numeros_inp[l]%2 == 0){
+            break;
+        } else {
+            printf("%d\t\t\t%d\n", numeros_par[l], numeros_inp[l]);
+        }
+        
+        l++;
     }
+    
+    /*for(l = 0 ; l <=50 ; l++){
+        printf("%d\t%d\n",numeros_par[l],numeros_inp[l]);
+    }*/
 
     return 0;
 }
